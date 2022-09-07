f03e1d295391c8e48bc848d72312d2a1cb879fc5
Youtube Taken IV Granted ✔ <dylanromero94@gmail.com>
2022-09-06 20:52:16 -0500

    Intial Commit

---
+++ click/clickapp/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    avatar = models.FileField(upload_to="images/", blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+class CommentSection(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
+    body = models.TextField(max_length=300)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
+    
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    comment = models.ForeignKey(CommentSection)
+
+
+class TvShow(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey('User', related_name='favorites')
+    show = models.ForeignKey('TvShow', related_name='favorites')
+
+    def __str__(self):
+        return self.show
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User)
+    favorites = models.ForeignKey(Favorite)
